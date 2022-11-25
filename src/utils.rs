6e99a71fdeea9c48ff4f611ec3f1e3e0aa5cb664
Niko Abeler <niko@rerere.org>
2022-11-25 20:11:55 +0100

    slightly better chunks

--- src/utils.rs
+++ src/utils.rs
@@ -3,11 +3,18 @@ use std::ops::Range;
 pub fn gen_chunks(n: usize, chunks: usize) -> Vec<Range<usize>> {
     let mut borders = vec![];
     let chunk_size = n / chunks;
+    let mut remainder = n % chunks;
     let mut start = 0;
     for _ in 0..chunks {
         let end = start + chunk_size;
-        borders.push(start..end);
-        start = end;
+        if remainder > 0 {
+            remainder -= 1;
+            borders.push(start..end + 1);
+            start = end + 1;
+        } else {
+            borders.push(start..end);
+            start = end;
+        }
     }
     // Change the last chunk to include the remainder
     let size = borders.len();
@@ -22,7 +29,7 @@ mod test {
     #[test]
     fn test_gen_chunks() {
         let borders = gen_chunks(10, 3);
-        assert_eq!(borders, vec![(0..3), (3..6), (6..10)]);
+        assert_eq!(borders, vec![(0..4), (4..7), (7..10)]);
     }
 
     #[test]
@@ -35,5 +42,11 @@ mod test {
     fn test_gen_chunks3() {
         let borders = gen_chunks(10, 1);
         assert_eq!(borders, vec![(0..10)]);
+    }
+
+    #[test]
+    fn test_gen_chunks4() {
+        let borders = gen_chunks(11, 3);
+        assert_eq!(borders, vec![(0..4), (4..8), (8..11)]);
     }
 }
